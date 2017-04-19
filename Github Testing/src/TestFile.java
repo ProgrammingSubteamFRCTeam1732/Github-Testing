372e6e7fa1be7f0fc0ddbe3e68565ddbf18f8f77
JayShower <lacrossejason@gmail.com>
2017-04-19 12:54:12 -0500

    Testing merging

--- Github Testing/src/TestFile.java
+++ Github Testing/src/TestFile.java
@@ -2,7 +2,10 @@ public class TestFile {
 
 	// change 1 - in master, in JayShower's fork of the repo, made project, this
 	// file, and this comment
-	
-	// change 2 - This is in ProgrammingSubteam's repo, testing to see if the way JayShower has his system configured will pull this change
 
+	// change 2 - This is in ProgrammingSubteam's repo, testing to see if the
+	// way JayShower has his system configured will pull this change
+
+	// change 4 - JayShower making a change to see what happens if two people
+	// make mergable changes
 }
