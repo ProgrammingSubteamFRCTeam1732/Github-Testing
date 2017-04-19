8be40a5dd0caf313e02331277bb181632a9e21e9
Team 1732 <programming@team1732.com>
2017-04-19 12:51:25 -0500

    Testing merging

--- Github Testing/src/TestFile.java
+++ Github Testing/src/TestFile.java
@@ -5,4 +5,5 @@ public class TestFile {
 	
 	// change 2 - This is in ProgrammingSubteam's repo, testing to see if the way JayShower has his system configured will pull this change
 
+	// change 3 - ProgrammingSubteam making a change to see what happens if people make mergable changes
 }
