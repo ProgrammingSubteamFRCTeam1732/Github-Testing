4fad9ede1ce797030e3ccaca33ec078c46599c6f
Team 1732 <programming@team1732.com>
2017-04-19 12:49:42 -0500

    Testing JayShower pulling

--- Github Testing/src/TestFile.java
+++ Github Testing/src/TestFile.java
@@ -2,5 +2,7 @@ public class TestFile {
 
 	// change 1 - in master, in JayShower's fork of the repo, made project, this
 	// file, and this comment
+	
+	// change 2 - This is in ProgrammingSubteam's repo, testing to see if the way JayShower has his system configured will pull this change
 
 }
