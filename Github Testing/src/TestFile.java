19c5510ebf1b70bac26ad0f4e45ae47cee4b4b7a
jchrist1732 <chrijohn@onid.oregonstate.edu>
2018-08-15 20:07:38 -0500

    Hopefully fixing merge issues...

---
+++ Github Testing/src/TestFile.java
@@ -0,0 +1,8 @@
+public class TestFile {
+
+	// change 1 - in master, in JayShower's fork of the repo, made project, this
+	// file, and this comment
+
+	// change 3 - ProgrammingSubteam making a change to see what happens if people make mergable changes
+
+}
