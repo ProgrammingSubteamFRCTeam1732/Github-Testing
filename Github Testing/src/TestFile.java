c3155d11564e08520dafb83db9e8c87d79790c5a
justinchan1732 <justinchan@Justins-MacBook-Pro.local>
2018-08-15 20:09:03 -0500

    fixed?

---
+++ Github Testing/src/TestFile.java
@@ -0,0 +1,10 @@
+public class TestFile {
+
+	// change 1 - in master, in JayShower's fork of the repo, made project, this
+	// file, and this comment
+
+	// change 3 - ProgrammingSubteam making a change to see what happens if people make mergable changes
+
+	// Test change 8/13/2018
+	// JC's second change. soon to be reverted back.
+}
